1e767c5cd05208d75c9503424868aa5cc81e6ddc
Susana vik <susana.m.vik@gmail.com>
2021-02-01 18:28:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,18 +1,18 @@
-def mario
+def mario(status_1)
   status_1 = "It's-a me, Mario!"
   puts status_1
 end
 
-def toadstool
+def toadstool(status_2)
   status_2 = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status_2
 end
 
-def link
+def link(status_3)
     status_3 = 'It's Dangerous To Go Alone! Take This'
   puts status_3
 end
 
-mario
-toadstool
-link
+mario(status_1)
+toadstool(status_2)
+link(status_3)
