92a61983077436f6ca127d3820b61ab4e6a1134e
Susana vik <susana.m.vik@gmail.com>
2021-02-01 18:27:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,16 +1,16 @@
 def mario
-  status = "It's-a me, Mario!"
-  puts status
+  status_1 = "It's-a me, Mario!"
+  puts status_1
 end
 
 def toadstool
-  status = "Thank You Mario! But Our Princess Is In Another Castle!"
-  puts status
+  status_2 = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts status_2
 end
 
 def link
-    status = 'It's Dangerous To Go Alone! Take This'
-  puts status
+    status_3 = 'It's Dangerous To Go Alone! Take This'
+  puts status_3
 end
 
 mario
